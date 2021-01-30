57b4f5c6fd473bd31a838cbb140befba5fc191da
SuhailRafi <suhailrafi9@gmail.com>
2021-01-30 15:32:41 +0600

    first commit

---
+++ BudgetApp/src/dao/UserDao.java
@@ -0,0 +1,9 @@
+package dao;
+
+import model.User;
+
+public interface UserDao {
+	
+	public boolean add(User user);
+	public boolean checkUser(String email,String pass);
+}
---
+++ BudgetApp/src/dao/UserIncomeDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import java.util.List;
+
+import model.UserExpense;
+import model.UserIncome;
+
+public interface UserIncomeDao {
+	
+	public boolean addIncome(UserIncome exp);
+	public List<UserIncome> getIncomeList(String email);
+}
